78c185f6b76d06bbf4629e33aa3ca40f9d46885b
faiz <mohdfaiz63226479@gmail.com>
2025-11-24 14:00:34 +0530

    tree question added

--- STEP 13 BINARY TREES/extra dfs/285_inorder_pred/successor.cpp
+++ STEP 13 BINARY TREES/extra dfs/285_inorder_pred/successor.cpp
@@ -47,3 +47,78 @@ class Solution {
         
     }
 };
+
+
+
+
+class Solution {
+public:
+    // Function to find the inorder 
+    // successor of a node in a BST
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        // Store the inorder traversal of the tree
+        vector<int> inorder;
+        // Perform in-order traversal to
+        // generate the sorted list
+        inorderTraversal(root, inorder);
+        
+        // Binary search to find the
+        // index of the node's value
+        int idx = binarySearch(inorder, p->val);
+        
+        // If index is out of range or it's the
+        // last element, return nullptr
+        if (idx == inorder.size() - 1 || idx == -1) {
+            return nullptr;
+        }
+        
+        // Return the next element
+        // as the inorder successor
+        return new TreeNode(inorder[idx + 1]);
+    }
+    
+    // Function to perform in-order traversal
+    // and store in 'inorder' vector
+    void inorderTraversal(TreeNode* root, vector<int>& inorder) {
+        // Base case: If the node is NULL, return
+        if (root == NULL) {
+            return;
+        }
+        
+        // Traverse left subtree
+        inorderTraversal(root->left, inorder);
+        
+        // Store current node's value
+        // in 'inorder' vector
+        inorder.push_back(root->val);
+        
+        // Traverse right subtree
+        inorderTraversal(root->right, inorder);
+    }
+    
+    // Function to perform
+    // binary search on array
+    int binarySearch(vector<int>& arr, int target) {
+        // Initialize pointers for binary search
+        int left = 0;
+        int right = arr.size() - 1;
+        
+        // Binary search algorithm to find the
+        // index of the target value
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // If target found, return the index
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // If target not found,
+        // return the index for insertion
+        return left == arr.size() ? -1 : left;
+    }
+};
