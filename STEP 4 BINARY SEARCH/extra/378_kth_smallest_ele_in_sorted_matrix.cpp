d1a98a461ffa01a45b4ec6cb10f79f86795ede38
faiz <mohdfaiz63226479@gmail.com>
2025-11-29 09:29:24 +0530

    bs  added

---
+++ STEP 4 BINARY SEARCH/extra/378_kth_smallest_ele_in_sorted_matrix.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+// class Solution {
+// public:
+//     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+//         priority_queue<int>pq;
+//         for(int i = 0;i < n; i++){
+//             for(int j = 0; j < m ; j++){
+//                 if(pq.size() <k)
+//                 pq.push(matrix[i][j]);
+//                 else{
+//                     if(pq.top()>matrix[i][j]){
+//                         pq.pop();
+//                         pq.push(matrix[i][j]);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return pq.top();
+//     }
+// };
+
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int low = matrix[0][0];
+        int high = matrix[n-1][m-1];
+
+        while(low <= high){
+            int cnt = 0;
+            int mid = low + (high - low )/2;
+
+            for(int i = 0; i < n; i++){
+                cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+            }
+
+            if(cnt < k) low = mid + 1;
+            else high = mid - 1;
+
+        }
+        return low;
+    }
+};
