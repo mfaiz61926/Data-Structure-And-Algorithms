4f7c169273cb10eee7b35544de867f276118b152
faiz <mohdfaiz63226479@gmail.com>
2025-12-16 23:24:54 +0530

    dp  added

--- STEP 16 DYNAMIC PROGRAMMING/extra/140_word_breakII.cpp
+++ STEP 16 DYNAMIC PROGRAMMING/extra/140_word_breakII.cpp
@@ -34,3 +34,31 @@ public:
         return dfs(0, s);
     }
 };
+
+//tabulation
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+
+        vector<vector<string>> dp(n + 1);
+        dp[n].push_back("");  // base case
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int l = 1; i + l <= n; l++) {
+                string word = s.substr(i, l);
+                if (dict.count(word)) {
+                    for (string &tail : dp[i + l]) {
+                        if (tail.empty())
+                            dp[i].push_back(word);
+                        else
+                            dp[i].push_back(word + " " + tail);
+                    }
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
