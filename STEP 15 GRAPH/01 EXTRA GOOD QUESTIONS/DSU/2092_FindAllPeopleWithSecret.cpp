f163e16ae2134ffbf23e4daca4d05ad873d990e6
faiz <mohdfaiz63226479@gmail.com>
2025-12-20 01:25:42 +0530

    dsu hard  added

--- STEP 15 GRAPH/01 EXTRA GOOD QUESTIONS/DSU/2092_FindAllPeopleWithSecret.cpp
+++ STEP 15 GRAPH/01 EXTRA GOOD QUESTIONS/DSU/2092_FindAllPeopleWithSecret.cpp
@@ -1,66 +1,100 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+class DisjointSet {
+    vector<int> rank, parent, size;
 public:
-    vector<int> parent, rankv;
-
-    int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    void unite(int a, int b) {
-        int pa = find(a), pb = find(b);
-        if (pa == pb) return;
-
-        if (rankv[pa] < rankv[pb]) parent[pa] = pb;
-        else if (rankv[pa] > rankv[pb]) parent[pb] = pa;
-        else {
-            parent[pb] = pa;
-            rankv[pa]++;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
-    void reset(int x) {
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+
+    void reset(int x){
         parent[x] = x;
-        rankv[x] = 0;
+        rank[x] = 0;
+    }
+};
+
+class Solution {
+public:
+    bool static comp(vector<int>&a, vector<int>&b){
+        return a[2] < b[2];
     }
 
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        sort(meetings.begin(), meetings.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[2] < b[2];
-             });
+        DisjointSet ds(n);
 
-        parent.resize(n);
-        rankv.assign(n, 0);
-        for (int i = 0; i < n; i++) parent[i] = i;
+        sort(meetings.begin(), meetings.end(), comp);
 
-        unite(0, firstPerson);
-
-        int i = 0, m = meetings.size();
-
-        while (i < m) {
+        ds.unionByRank(0, firstPerson);
+        int i =0;
+        int m = meetings.size();
+        while(i < m){
             int time = meetings[i][2];
-            vector<int> list;
-
-            while (i < m && meetings[i][2] == time) {
-                unite(meetings[i][0], meetings[i][1]);
+            vector<int>list;
+            while(i < m && meetings[i][2] == time){
+                ds.unionByRank(meetings[i][0], meetings[i][1]);
                 list.push_back(meetings[i][0]);
                 list.push_back(meetings[i][1]);
                 i++;
             }
-
-            for (int p : list) {
-                if (find(0) != find(p)) {
-                    reset(p);
+            for(auto &it : list){
+                if(ds.findUPar(0) != ds.findUPar(it)){
+                    ds.reset(it);
                 }
             }
         }
 
-        vector<int> ans;
-        for (int p = 0; p < n; p++) {
-            if (find(0) == find(p)) ans.push_back(p);
+        vector<int>ans;
+
+        for(int i = 0; i < n; i++){
+            if(ds.findUPar(0) == ds.findUPar(i)){
+                ans.push_back(i);
+            }
         }
+
         return ans;
     }
 };
