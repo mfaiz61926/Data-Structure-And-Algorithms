b13d8c7f3fc6240220a4bd8582c46653b8eac6e1
faiz <mohdfaiz63226479@gmail.com>
2026-06-15 12:37:02 +0530

    binary tree problem  added

---
+++ STEP 13 BINARY TREES/extra dfs/Deletion_in_aBinaryTree.cpp
@@ -0,0 +1,62 @@
+/*
+Structre of the node of the tree is as
+struct Node
+{
+    int data;
+    Node* left, *right;
+};
+*/
+class Solution {
+public:
+    Node* deleteNode(Node* root, int key) {
+        if (!root) return nullptr;
+
+        if (!root->left && !root->right) {
+            if (root->data == key) {
+                delete root;
+                return nullptr;
+            }
+            return root;
+        }
+
+        queue<Node*> q;
+        q.push(root);
+
+        Node* target = nullptr;
+        Node* deepest = nullptr;
+        Node* parent = nullptr;
+
+        while (!q.empty()) {
+            Node* cur = q.front();
+            q.pop();
+
+            if (cur->data == key)
+                target = cur;
+
+            if (cur->left) {
+                parent = cur;
+                q.push(cur->left);
+            }
+
+            if (cur->right) {
+                parent = cur;
+                q.push(cur->right);
+            }
+
+            deepest = cur;
+        }
+
+        if (!target) return root;
+
+        target->data = deepest->data;
+
+        if (parent->right == deepest)
+            parent->right = nullptr;
+        else
+            parent->left = nullptr;
+
+        delete deepest;
+
+        return root;
+    }
+};
