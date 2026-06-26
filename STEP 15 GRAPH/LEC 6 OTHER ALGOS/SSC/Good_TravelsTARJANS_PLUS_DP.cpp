6ab04e78946468decc70895911e4f54c24584e7b
faiz <mohdfaiz63226479@gmail.com>
2026-06-26 12:20:54 +0530

    TARJAN'S + DP  ALGO CSES

---
+++ STEP 15 GRAPH/LEC 6 OTHER ALGOS/SSC/Good_TravelsTARJANS_PLUS_DP.cpp
@@ -0,0 +1,259 @@
+/******************************************************************************
+
+                              بسم الله الرحمان الرحيم
+Author:mconq(Mohd_Faiz)
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+
+#define fr0(i,n)       for(int i=0; i<(n); i++)
+#define fr1(i,n)       for(int i = 1; i <= n; i++)
+#define fra(i,a,n)     for(int i=(a); i<=(n); i++)
+
+#define srT(v)         sort(v.begin(),v.end())         // sort 
+#define mxE(v)         *max_element(v.begin(),v.end())     // find max element in vector
+#define mne_3(v)       *min_element(v.begin(),v.end())     // find min element in vector
+#define double         long double
+#define pq_mx          priority_queue <int>
+#define pq_mn          priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+// my love;
+#define permu(p, a)    iota(all(p), a)
+
+
+int MOD=1e9+7; 
+void mod_add(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void mo_dsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void mo_dmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1) x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+//lin one start here
+// ==================== custom to_string helpers ====================
+
+string to_string(char c) {
+    return string(1, c);
+}
+
+string to_string(bool b) {
+    return b ? "true" : "false";
+}
+
+string to_string(const char* s) {
+    return string(s);
+}
+
+string to_string(const string& s) {
+    return s;
+}
+
+string to_string(const vector<bool>& v) {
+    string res;
+    for (bool x : v)
+        res += char('0' + x);
+    return res;
+}
+
+// ================================================================
+
+struct fenwick {
+    int n;
+    vector<int> fn;
+
+    void init(int n) {
+        this->n = n;
+        fn.assign(n + 1, 0);
+    }
+
+    void add(int x, int val) {
+        x++;
+        while (x <= n) {
+            fn[x] += val;
+            x += (x & -x);
+        }
+    }
+
+    int sum(int x) {
+        x++;
+        int res = 0;
+        while (x > 0) {
+            res += fn[x];
+            x -= (x & -x);
+        }
+        return res;
+    }
+
+    int sum(int l, int r) {
+        if (l > r) return 0;
+        return sum(r) - sum(l - 1);
+    }
+};
+
+const int d4r[4]={-1, 0, 1, 0}, d4c[4]={0, 1, 0, -1};
+const int d8r[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8c[8]={0, 1, 1, 1, 0, -1, -1, -1};
+const int N = 1000005;
+
+vector<int>adj[N], dag[N];
+vector<vector<int>>comp;
+int tin[N], low[N];
+stack<int>st;
+bool inStack[N];
+int fun[N], compId[N], dp[N], compFun[N];
+int timer, scc;
+int S,E;
+int vis[N];
+
+void dfs(int u){
+    tin[u] = low[u] = ++timer;
+    st.push(u);
+    inStack[u] = true;
+
+    for(int v : adj[u]){
+        if(!tin[v]){
+            dfs(v);
+            low[u] = min(low[u], low[v]);
+        }
+        else if(inStack[v]){
+            low[u] = min(low[u], tin[v]);
+        }
+    }
+
+    if(low[u] == tin[u]){
+        scc++;
+        comp.push_back({});
+        int v = -1;
+
+        while(v != u){
+            v = st.top();
+            st.pop();
+            comp.back().push_back(v);
+            compId[v] = scc;
+            compFun[scc] += fun[v];
+            inStack[v] = false;
+        }
+    }
+}
+
+int dfs1(int u){
+    int &ret = dp[u];
+    if(vis[u]){
+        return ret;
+    }
+    vis[u] = 1;
+    for(auto it : dag[u]){
+        ret = max(ret, compFun[u] + dfs1(it));
+    }
+    return ret;
+}
+
+void m_conq() {
+        int n, m, s, e;
+        cin >> n >> m >> s >> e;
+
+        for(int i = 1; i <= n; i++){
+            cin >> fun[i];
+        }
+
+        for(int i = 0; i < m; i++){
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        for(int u = 1; u <= n; u++){
+            if(!tin[u]){
+                dfs(u);
+            }
+        }
+
+        for(int u = 1; u <= n; u++){
+            for(int v : adj[u]){
+                if(compId[u] != compId[v]){
+                    dag[compId[u]].push_back(compId[v]);
+                }
+            }
+        }
+
+        //dp
+        S = compId[s];
+        E = compId[e];
+        dp[E] = compFun[E];
+        vis[E] = 1;
+        dfs1(S);
+
+        cout << dp[S] << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt",  "r",  stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    // clock_t z = clock();
+
+    int t = 1;
+    // cin >> t;
+    fr1(i, t){
+        // cout << "Case #" <<  i+1 << ": ";
+        m_conq();
+    }
+
+    // cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+}
